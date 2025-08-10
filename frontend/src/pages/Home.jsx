29b70da35cba6e58cf4b36d6af4bda6c282f5719
Raj Suriyan G <rajsuriyan200110@gmail.com>
2025-08-10 23:03:05 +0530

    Add Home page functions

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,22 @@
+import { useEffect, useState } from "react";
+import api from "../api";
 function Home(){
+    const [note,setNote] = useState([]);
+    const [content,setContent] = useState("");
+    const [title,setTitle] = useState(""); 
+
+    useEffect(()=>{
+        getNotes();
+    },[])
+
+    const getNotes = () => {
+        api
+        .get("/api/notes/")
+        .then((res) => res.data)
+        .then((data) => {setNote(data);console.log(data)})
+        .catch((err)=> alert(err))
+    }
+    const deleteNote = () => {}
     return <div>Home</div>
 }
 
