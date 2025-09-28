f649f7a1f9d7a986b547e29888668696ec7604ba
Raj Suriyan G <rajsuriyan200110@gmail.com>
2025-09-28 19:50:25 +0530

    Add allowded origins

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -136,7 +136,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+CORS_ALLOWED_ORIGINS = [
+    "https://my-frontend.onrender.com",
+    "http://localhost:5173"
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
